2d718f5ce112800a966e6f88327a77e20b7f3528
JinnTao <314315805@qq.com>
2019-03-11 18:13:42 +0800

    Td 登录

--- src/datasource/ctptdspi.cpp
+++ src/datasource/ctptdspi.cpp
@@ -2,14 +2,43 @@
 
 #include "utils/logger.h"
 
-void ctpTdSpi::OnFrontConnected() {}
+void ctpTdSpi::OnFrontConnected() {
 
-void ctpTdSpi::OnFrontDisconnected(int nReason) {}
+
+}
+
+void ctpTdSpi::OnFrontDisconnected(int nReason) {
+
+}
 
 void ctpTdSpi::OnRspUserLogin(CThostFtdcRspUserLoginField* pRspUserLogin,
                               CThostFtdcRspInfoField*      pRspInfo,
                               int                          nRequestID,
-                              bool                         bIsLast) {}
+                              bool                         bIsLast) {
+                                // std::lock_guard<std::mutex> guard(mut_);
+                                if (!IsErrorRspInfo(pRspInfo) && pRspUserLogin) {
+                                    m_FRONT_ID        = pRspUserLogin->FrontID;
+                                    m_SESSION_ID      = pRspUserLogin->SessionID;
+                                    int iNextOrderRef = atoi(pRspUserLogin->MaxOrderRef);
+                                    m_tradeDay        = string(pRspUserLogin->TradingDay);
+                                    m_actionDay       = "";  // cSystem::GetCurrentDayBuffer();
+
+                                    iNextOrderRef++;
+                                    sprintf(m_ORDER_REF, "%d", iNextOrderRef);
+
+                                    ILOG("cTraderSpi::OnRspUserLogin,TradeDate:{},SessionID:{},FrontID:{},MaxOrderRef:{}.",
+                                        pRspUserLogin->TradingDay,
+                                        pRspUserLogin->SessionID,
+                                        pRspUserLogin->FrontID,
+                                        pRspUserLogin->MaxOrderRef);
+                                }
+                                if (bIsLast) {
+                                    if (on_login_fun_) {
+                                        std::invoke(on_login_fun_, pRspUserLogin, pRspInfo);
+                                    }
+                                }
+
+                              }
 
 void ctpTdSpi::OnRspSettlementInfoConfirm(CThostFtdcSettlementInfoConfirmField* pSettlementInfoConfirm,
                                           CThostFtdcRspInfoField*               pRspInfo,
